51ef324fb6aa8ce486c9c61c7b8724b6a0cbf602
Luke Lefebure <lefebure@stanford.edu>
2017-08-13 15:58:44 -0700

    make knit post easier to use

--- utilities/knit_post.R
+++ utilities/knit_post.R
@@ -1,56 +1,29 @@
 # adapted from Andrew Brooks
 # (http://brooksandrew.github.io/simpleblog/articles/blogging-with-r-markdown-and-jekyll-using-knitr/)
 
-KnitPost <- function(site.path='~/Documents/Projects/llefebure.github.io/', rmd.path='',
-                     overwriteAll=F, overwriteOne=NULL) {
-  if(!'package:knitr' %in% search()) library('knitr')
+# Dump Rmd file into _R/<file_name>.Rmd and run KnitPost(<file_name>.Rmd)
+
+KnitPost <- function(rmd.name) {
   
-  ## Blog-specific directories.  This will depend on how you organize your blog.
-  site.path <- site.path # directory of jekyll blog (including trailing slash)
-  rmd.path <- paste0('~/Documents/Projects/llefebure.github.io/_R/', rmd.path)
-  fig.dir <- "assets/Rfig/" # directory to save figures
+  site.path <- '~/Documents/Projects/llefebure.github.io/'
+  
+  library(knitr)
+  library(stringr)
+  
+  ## Blog-specific directories. This will depend on how you organize your blog.
+  rmd.path <- paste0(site.path, "_R/", rmd.name) # file path for Rmd to knit
+  fig.dir <- paste0("assets/Rfig/") # directory to save figures
   posts.path <- paste0(site.path, "_posts/") # directory for converted markdown files
   cache.path <- paste0(site.path, "_cache") # necessary for plots
   
   render_jekyll(highlight = "pygments")
   opts_knit$set(base.url = '/', base.dir = site.path)
-  opts_chunk$set(fig.path=fig.dir, fig.width=8.5, fig.height=5.25, dev='svg', cache=F, 
-                 warning=F, message=F, cache.path=cache.path, tidy=F)   
+  opts_chunk$set(fig.path=fig.dir, fig.width=8.5, fig.height=5.25, cache=F, 
+                 warning=F, message=F, cache.path=cache.path, dev = "svg", tidy=F)   
   
-  
-  setwd(rmd.path) # setwd to base
-  
-  # some logic to help us avoid overwriting already existing md files
-  files.rmd <- data.frame(rmd = list.files(path = rmd.path,
-                                           full.names = T,
-                                           pattern = "\\.Rmd$",
-                                           ignore.case = T,
-                                           recursive = F), stringsAsFactors=F)
-  files.rmd$corresponding.md.file <- paste0(posts.path, "/", basename(gsub(pattern = "\\.Rmd$", replacement = ".md", x = files.rmd$rmd)))
-  files.rmd$corresponding.md.exists <- file.exists(files.rmd$corresponding.md.file)
-  
-  ## determining which posts to overwrite from parameters overwriteOne & overwriteAll
-  files.rmd$md.overwriteAll <- overwriteAll
-  if(is.null(overwriteOne)==F) files.rmd$md.overwriteAll[grep(overwriteOne, files.rmd[,'rmd'], ignore.case=T)] <- T
-  files.rmd$md.render <- F
-  for (i in 1:dim(files.rmd)[1]) {
-    if (files.rmd$corresponding.md.exists[i] == F) {
-      files.rmd$md.render[i] <- T
-    }
-    if ((files.rmd$corresponding.md.exists[i] == T) && (files.rmd$md.overwriteAll[i] == T)) {
-      files.rmd$md.render[i] <- T
-    }
-  }
-  
-  # For each Rmd file, render markdown (contingent on the flags set above)
-  for (i in 1:dim(files.rmd)[1]) {
-    if (files.rmd$md.render[i] == T) {
-      out.file <- knit(as.character(files.rmd$rmd[i]), 
-                       output = as.character(files.rmd$corresponding.md.file[i]),
-                       envir = parent.frame(), 
-                       quiet = T)
-      message(paste0("KnitPost(): ", basename(files.rmd$rmd[i])))
-    }     
-  }
-  
+  # render markdown
+  out.file <- knit(rmd.path,
+                   output = paste0(posts.path, str_replace(rmd.name, ".Rmd", ".md")),
+                   envir = parent.frame(), 
+                   quiet = T)
 }
